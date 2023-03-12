5e35e9d88a592a35a0b83a086774cd55e1331098
Phil <4522927+p2635@users.noreply.github.com>
2023-03-12 17:21:10 +0000

    Removed dev comments

--- test/askui-login-firefox.test.ts
+++ test/askui-login-firefox.test.ts
@@ -3,9 +3,9 @@
   By: Philip Wong
   Created: 5 March 2023
   Test Environment:
-  Windows 10 (64-bit), 125% text size
-  1920 x 1080 screen resolution
-  Firefox Browser v110.0.1 (64-bit)
+  * Windows 10 (64-bit), 125% text size
+  * 1920 x 1080 screen resolution
+  * Firefox Browser v110.0.1 (64-bit)
 */
 import { aui } from './helper/jest.setup';
 
@@ -16,10 +16,6 @@ const FULL_NAME = 'Phil Wong'
 const WORKSPACE_NAME = 'testing'
 
 describe('Launch Firefox in private mode', () => {
-
-  // it('', async () => {
-  //   await aui.annotate();
-  // });
 
   it('should launch Windows Run prompt', async () => {
     await aui.pressTwoKeys('command', 'R').exec();
@@ -65,13 +61,7 @@ describe('Set Firefox to Zoom text only 200%', () => {
   });
 
   it('should toggle on zoom text only', async () => {
-    // The box should not be checked initially, but these steps are flaky.
-    // await aui.expect().icon().withText("stop").exists().exec();
-    // flaky - aui.expect().icon().withText("check").notExists().exec();    
     await aui.pressThreeKeys('alt', 'shift', 't').exec();
-
-    // The box should be checked but this step is flaky.
-    // await aui.expect().icon().withText("check").exists().exec();
     await aui.expect().icon().withText("stop").notExists().exec();
   });
 
@@ -90,12 +80,6 @@ describe('Keyboard user logs in to askui', () => {
   });
 
   it('should dismiss the cookie notice', async () => {
-
-    /*
-      pressKeyXTimes('tab', 10); This does not work.
-      I had to take this out because the focus is not
-      in the cookie notice. It's in the background. 
-    */
     await aui.click().text().withText('Deny').exec();
 
     await aui.expect().text().withText("Log in").exists().exec();
@@ -129,51 +113,5 @@ describe('Keyboard user logs in to askui', () => {
     await aui.expect().text().withText(WORKSPACE_NAME).exists().exec();
   });
 
-  it('not-so-obvious signal to indicate tests have completed', async () => {
-    await aui.mouseRightClick().exec();
-  });
-
 });
 
-/*
-  Old code no longer used because it didn't quite work.
-
-  // Helper functions
-  It seems that this is nice syntactic sugar but in practice, it's LESS
-  reliable that just copying `await aui.pressKey().exec()` a bunch of times. Such a shame. I have no idea why that might be the case.
-
-  async function pressKeyXTimes(key, x, delay = 0){
-    for (let i = 0; i < x; i++) {
-      await aui.pressKey(key).exec();
-      await aui.waitFor(delay).exec();
-    }
-  }
-  async function pressTwoKeysXTimes(key1, key2, x){
-    for (let i = 0; i < x; i++) {
-      await aui.pressTwoKeys(key1, key2).exec();
-    }
-  }
-
-  // Configure zoom level by 'View' menu
-  xit('should set zoom to 200%', async () => {
-    // This doesn't work - pressTwoKeysXTimes('control', '+', 6);
-    for (let i = 0; i < 6; i++) {
-      await aui.pressTwoKeys('alt', 'v').exec();
-      await aui.pressKey('z').exec();
-      await aui.pressKey('i').exec();
-    }
-    await aui.expect().text().withText("200%").exists().exec();
-  });
-
-  // Configure zoom text only by 'View' menu
-  xit('should toggle zoom text only on', async () => {
-    await aui.pressKey('t').exec();
-
-    await aui.pressTwoKeys('alt', 'v').exec();
-    await aui.pressKey('z').exec();
-    await aui.expect().icon().withText("check").exists().exec();
-    await aui.pressKey('alt').exec(); // To close the menu items
-    await aui.expect().text().withExactText("Zoom").notExists().exec();
-  });
-
-*/
